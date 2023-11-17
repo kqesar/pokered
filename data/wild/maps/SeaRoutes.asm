bf49a3d3be68cab62bcabc5c693c8e2c0cc0fb6b
kqesar <yoann.fievez@gmail.com>
2023-11-17 18:47:16 +0100

    fix: remove error wild pokemon for sea routes

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -1,26 +1,5 @@
 SeaRoutesWildMons:
-	def_grass_wildmons 20 ; encounter rate
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
-	db 120, MISSINGNO
+	def_grass_wildmons 0 ; encounter rate
 	end_grass_wildmons
 
-	def_water_wildmons 7 ; encounter rate
-	db 32, TENTACOOL
-	db 30, TENTACOOL
-	db 28, HORSEA
-	db 30, STARYU
-	db 31, HORSEA
-	db 30, STARYU
-	db 35, MAGIKARP
-	db 25, GYARADOS
-	db 32, TENTACRUEL
-	db 36, TENTACRUEL
-	end_water_wildmons
+	def_water_wildmons 5 ; encounter rate
